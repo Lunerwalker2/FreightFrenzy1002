d721ac8da0a9c900473c85ce7957d670d0b816a8
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2021-10-10 22:11:07 -0400

    feat: added the eocv pipelines to master

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/TeamMarkerPipelineTester.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.Vision;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Vision.pipeline.TeamMarkerPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@TeleOp(name="Team Marker Pipeline Tester")
+public class TeamMarkerPipelineTester extends LinearOpMode {
+
+
+    OpenCvWebcam webcam;
+
+    TeamMarkerPipeline pipeline = new TeamMarkerPipeline();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //Get the viewport id
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        //Create the camera object, for us a webcam
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        //Set the pipeline
+        webcam.setPipeline(pipeline);
+
+        webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+
+        telemetry.addLine("Ready, waiting for start");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            if (isStopRequested()) return;
+
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+
+            telemetry.addData("Current Hub Level", pipeline.getHubLevel());
+
+            telemetry.update();
+
+        }
+
+    }
+}
