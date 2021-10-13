a154d7843a5d61f15132ecefeefcef6cef32e6ec
Ryan <arizonaredline@gmail.com>
2021-10-13 17:30:32 -0400

    feat: Track width and follower pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.9685; // in
     public static double GEAR_RATIO = 0.9873830877; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 15.4; // in
+    public static double TRACK_WIDTH = 14.7; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -88,8 +88,8 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 40.0;
     public static double MAX_ACCEL = 40.0;
-    public static double MAX_ANG_VEL = Math.toRadians(203.39462337662337);
-    public static double MAX_ANG_ACCEL = Math.toRadians(203.39462337662337);
+    public static double MAX_ANG_VEL = Math.toRadians(200);
+    public static double MAX_ANG_ACCEL = Math.toRadians(200);
 
 
     public static double encoderTicksToInches(double ticks) {
