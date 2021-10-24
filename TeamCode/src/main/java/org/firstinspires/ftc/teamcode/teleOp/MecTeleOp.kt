da7488bdbff1594f2843e17ed9677cff2008072b
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2021-10-24 00:03:09 -0400

    fix: started mec tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/MecTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/MecTeleOp.kt
@@ -1,11 +1,35 @@
 package org.firstinspires.ftc.teamcode.teleOp
 
 import com.arcrobotics.ftclib.command.CommandOpMode
+import com.arcrobotics.ftclib.command.button.GamepadButton
+import com.arcrobotics.ftclib.gamepad.GamepadEx
+import com.arcrobotics.ftclib.gamepad.GamepadKeys
+import org.firstinspires.ftc.teamcode.subsystems.Arm
+import org.firstinspires.ftc.teamcode.subsystems.CarouselWheel
+import org.firstinspires.ftc.teamcode.subsystems.Intake
 
 class MecTeleOp : CommandOpMode() {
 
+    lateinit var arm: Arm
+    lateinit var intake: Intake
+    lateinit var carouselWheel: CarouselWheel
+
+    private var powerMultiplier = 1.0
 
     override fun initialize() {
+
+        arm = Arm(hardwareMap)
+        intake = Intake(hardwareMap)
+        carouselWheel = CarouselWheel(hardwareMap)
+
+        val driver = GamepadEx(gamepad1)
+        val manipulator = GamepadEx(gamepad2)
+
+
+        val slowModeButton = GamepadButton(driver, GamepadKeys.Button.LEFT_BUMPER, GamepadKeys.Button.RIGHT_BUMPER) //button to enable slow mode
+        slowModeButton.whenPressed( Runnable { powerMultiplier = 0.6})
+        slowModeButton.whenReleased( Runnable { powerMultiplier = 1.0})
+
 
     }
 
