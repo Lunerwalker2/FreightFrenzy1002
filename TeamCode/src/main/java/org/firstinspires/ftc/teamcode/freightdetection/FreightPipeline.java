b052a717957ee323139b3068b934632cd709c83e
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2021-11-18 22:00:47 -0500

    d

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightdetection/FreightPipeline.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.freightdetection;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class FreightPipeline extends OpenCvPipeline {
+
+
+
+
+
+    @Override
+    public Mat processFrame(Mat input){
+
+
+
+        return input;
+    }
+}
