b8d0064015b3a5bf8b8a16cca635724aecc346bc
Lunerwalker2 <arizonaredline@gmail.com>
2021-12-05 13:09:30 -0500

    fix: added teleop annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testing/IMUAxisDebugger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testing/IMUAxisDebugger.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.teleOp.testing;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
@@ -9,6 +10,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
+@TeleOp
 public class IMUAxisDebugger extends LinearOpMode {
 
     private BNO055IMU imu;
