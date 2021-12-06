d1a10bb70fa88c38ff62481dc43f8799d48cfa2a
Lunerwalker2 <arizonaredline@gmail.com>
2021-12-06 13:41:35 -0500

    fix: put a fake pipeline in the demo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightdetection/NotebookDemoPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightdetection/NotebookDemoPipeline.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.freightdetection;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
@@ -19,11 +20,29 @@ public class NotebookDemoPipeline extends OpenCvPipeline {
     public Mat processFrame(Mat input){
 
         Imgproc.cvtColor(input,channel, Imgproc.COLOR_RGB2YCrCb);
-        Core.extractChannel(channel, channel, 2);
-
-        Imgproc.putText(channel, "Cb channel", new Point(170, 30), 0, 0.8, new Scalar(88, 164, 200), 2);
 
 
-        return channel;
+        Imgproc.rectangle(input,
+                new Rect(
+                        (int) (0.25 * input.width()),
+                        (int) (0.55 * input.height()),
+                        20,
+                        20
+                ),
+                new Scalar(20.0, 220.0, 70.0), 2);
+
+        Imgproc.rectangle(input,
+                new Rect(
+                        (int) (0.6 * input.width()),
+                        (int) (0.55 * input.height()),
+                        20,
+                        20
+                ),
+                new Scalar(190.0, 40.0, 70.0), 2);
+
+        Imgproc.putText(channel, "Top level", new Point(170, 30), 0, 0.8, new Scalar(88, 164, 200), 2);
+
+
+        return input;
     }
 }
