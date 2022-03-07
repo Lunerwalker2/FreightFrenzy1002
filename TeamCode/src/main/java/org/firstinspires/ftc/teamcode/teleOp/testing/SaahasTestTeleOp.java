e589197a844b835ff82ed08ebdca578b51d98d24
Saahas Swaroop <saahas.swaroop@googlemail.com>
2022-03-07 12:04:13 -0500

    made a test tele-op
    tele-op that i made to test my skills. uwu

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testing/SaahasTestTeleOp.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.teleOp.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "TestTeleOp")
+public class SaahasTestTeleOp extends LinearOpMode {
+    DcMotor rightFront = hardwareMap.get(DcMotor.class, "rf");
+    DcMotor leftFront = hardwareMap.get(DcMotor.class, "rb");
+    DcMotor rightBack = hardwareMap.get(DcMotor.class, "lb");
+    DcMotor leftBack = hardwareMap.get(DcMotor.class, "lb");
+
+
+    @Override
+    public void runOpMode() {
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        while (true) {
+            if (gamepad1.a) {
+                rightFront.setPower(1);
+                rightBack.setPower(1);
+                leftFront.setPower(1);
+                leftBack.setPower(1);
+            }
+            else if (gamepad1.b) {
+                rightFront.setPower(-1);
+                rightBack.setPower(1);
+                leftFront.setPower(1);
+                leftBack.setPower(-1);
+            }
+            else if (gamepad1.x) {
+                rightFront.setPower(-1);
+                rightBack.setPower(-1);
+                leftFront.setPower(-1);
+                leftBack.setPower(-1);
+            }
+            else if (gamepad1.y) {
+                rightFront.setPower(1);
+                rightBack.setPower(-1);
+                leftFront.setPower(-1);
+                leftBack.setPower(1);
+            }
+        }
+    }
+}
