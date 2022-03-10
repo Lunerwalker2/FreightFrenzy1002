963e1705211c4ef60eacb74770c08c91f0678508
Saahas Swaroop <saahas.swaroop@googlemail.com>
2022-03-10 16:38:14 -0500

    aarush bad sus moment
    bill nye ur moms a guy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedDuckAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedDuckAuto.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@Autonomous
+@Autonomous(name = "redDuckAuto")
 public class RedDuckAuto extends LinearOpMode {
     @Override
     public void runOpMode() {
