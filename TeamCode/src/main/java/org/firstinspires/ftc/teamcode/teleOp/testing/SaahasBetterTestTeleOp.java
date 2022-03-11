ec1a6fbdf3eacbce640f8040f1ce742f24a274d2
Saahas Swaroop <saahas.swaroop@googlemail.com>
2022-03-11 11:14:24 -0500

    I CAN SCORE NOW ;ALKSLKASDJKLAS;FKL
    LOOK RYAN LOOK RYAN LOOK AT ME I CAN SCORE IM SOOOOO GOOD GET GOOD wait i didn't test this yet NVM IM JUST BUILT DIFFERENT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testing/SaahasBetterTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testing/SaahasBetterTestTeleOp.java
@@ -4,11 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "SkillIssueTeleOp")
 public class SaahasBetterTestTeleOp extends LinearOpMode {
-    DcMotor rightFront,leftFront,rightBack,leftBack,carouselMotor,frontIntake,backIntake;
+    DcMotor rightFront,leftFront,rightBack,leftBack,carouselMotor,frontIntake,backIntake, liftMotor;
     Servo backFlap,frontFlap,bucketServo;
 
     //initialize
@@ -30,6 +31,7 @@ public class SaahasBetterTestTeleOp extends LinearOpMode {
         carouselMotor = hardwareMap.get(DcMotor.class,"carouselMotor");
         frontIntake = hardwareMap.get(DcMotor.class, "frontIntake");
         backIntake = hardwareMap.get(DcMotor.class, "backIntake");
+        liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
         frontFlap = hardwareMap.get(Servo.class, "frontFlap");
         backFlap = hardwareMap.get(Servo.class, "backFlap");
         bucketServo = hardwareMap.get(Servo.class,"bucketServo");
@@ -42,12 +44,17 @@ public class SaahasBetterTestTeleOp extends LinearOpMode {
         while (opModeIsActive()) {
 
             A = gamepad1.a;
+            B = gamepad1.b;
+            X = gamepad1.x;
+            Y = gamepad1.y;
             dPadRight = gamepad1.dpad_right;
             dPadLeft = gamepad1.dpad_left;
             rightBumper = gamepad1.right_bumper;
             leftBumper = gamepad1.left_bumper;
             rightTrigger = gamepad1.right_trigger;
             leftTrigger = gamepad1.left_trigger;
+            DPADUP = gamepad1.dpad_up;
+            DPADDOWN = gamepad1.dpad_down;
 
 
             if (A && (COUNT % 2) == 1) {
@@ -116,6 +123,27 @@ public class SaahasBetterTestTeleOp extends LinearOpMode {
                     frontIntake.setPower(0);
                     backIntake.setPower(0);
                     setServoPosition(backFlap, 0);
+                }
+            }
+
+            if (opModeIsActive()) {
+                if (DPADUP) {
+                    liftMotor.setPower(.5);
+                }
+                else if (DPADDOWN) {
+                    liftMotor.setPower(-.5);
+                }
+                else {
+                    liftMotor.setPower(0);
+                }
+            }
+
+            if (opModeIsActive()) {
+                if (X) {
+                    bucketServo.setPosition(1);
+                }
+                else if (B) {
+                    bucketServo.setPosition(0);
                 }
             }
 
